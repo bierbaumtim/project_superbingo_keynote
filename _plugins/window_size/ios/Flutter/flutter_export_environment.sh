a2b5c4e1993a2ca9237cd97be97e176b9f3e3a65
tb2761 <tb2706@outlook.de>
2020-02-16 11:45:23 +0100

    added windowsize plugin for later experiments

---
+++ _plugins/window_size/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Tb270\Flutter\flutter_master"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Tb270\Git_Projects\flutter-desktop-embedding\plugins\window_size"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\Tb270\Flutter\flutter_master\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=0.0.1"
