fbe161621309696689bd5adfb7aeeb8315237236
tb2761 <tb2706@outlook.de>
2020-02-23 14:41:21 +0100

    refactor mobile container

---
+++ lib/widgets/mobil_container.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class MobileContainer extends StatelessWidget {
+  final Widget topChild;
+  final Widget backChild;
+  final BoxConstraints topChildConstraints;
+
+  const MobileContainer({
+    Key key,
+    this.topChild,
+    this.backChild,
+    this.topChildConstraints,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        if (backChild != null) backChild,
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xFF223044),
+              borderRadius: BorderRadius.circular(40),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.grey[850],
+                  blurRadius: 10,
+                ),
+              ],
+            ),
+            constraints: topChildConstraints,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(40),
+              child: topChild,
+            ),
+            margin: const EdgeInsets.all(10),
+          ),
+        ),
+      ],
+    );
+  }
+}
