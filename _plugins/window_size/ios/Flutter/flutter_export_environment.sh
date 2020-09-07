b0e82f826104dc369da743c4a58c450011960bad
Tim Bierbaum <bierbaum.tim@outlook.de>
2020-09-07 20:03:14 +0200

    remove windows utilsplugin

--- _plugins/window_size/ios/Flutter/flutter_export_environment.sh
+++ _plugins/window_size/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Tb270\Flutter\flutter_master"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Tb270\Git_Projects\flutter-desktop-embedding\plugins\window_size"
+export "FLUTTER_ROOT=C:\Users\Tim\flutter\flutter_stable"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Tim\Git_Projects\project_superbingo_keynote\_plugins\window_size"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\Tb270\Flutter\flutter_master\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\Tim\flutter\flutter_stable\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=0.0.1"
 export "FLUTTER_BUILD_NUMBER=0.0.1"
